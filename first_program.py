bf3f95a24b6e35821f93d7da6ad53c063be5b063
Sithan Kanna <sithankanna@gmail.com>
2020-11-01 13:33:46 +0000

    Wrap print statements in a function

--- first_program.py
+++ first_program.py
@@ -1,2 +1,7 @@
-print("First program running")
-print("twice, third timee")
+def main():
+    print("First program running")
+    print("twice, third time")
+    pass 
+
+if __name__ == '__main__':
+    main()
